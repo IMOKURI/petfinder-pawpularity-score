2bb2b2ca820dbce5ef9180c9bb34e9872006fbfc
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-10-15 17:07:39 +0900

    update

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import logging
+
 import hydra
 import mlflow
 
 import src.utils as utils
 
+log = logging.getLogger(__name__)
+
 
 @hydra.main(config_path="config", config_name="main")
-def main(config):
-    print(config)
+def main(c):
+    log.info("Started.")
 
-    mlflow.set_tracking_uri(config.mlflow.tracking_uri)
-    mlflow.set_experiment(config.mlflow.experiment)
+    mlflow.set_tracking_uri(c.mlflow.tracking_uri)
+    mlflow.set_experiment(c.mlflow.experiment)
 
     with mlflow.start_run():
-        utils.log_params_from_omegaconf_dict("params", config.params)
+        utils.log_params_from_omegaconf_dict("params", c.params)
+
+        log.info("Done.")
+        mlflow.log_artifacts(c.settings.output_dir)
+
 
 
 if __name__ == "__main__":
