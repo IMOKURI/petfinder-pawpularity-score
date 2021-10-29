531be0aab43a8a41545096b40b914db1adbd89c9
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-10-29 20:08:30 +0900

    update

--- src/get_score.py
+++ src/get_score.py
@@ -12,7 +12,7 @@ def get_score(y_true, y_pred):
     return np.sqrt(mean_squared_error(y_true, y_pred))
 
 
-def get_result(c, df, fold):
+def get_result(c, df, fold, loss=None):
     preds = df["preds"].values
     labels = df["Pawpularity"].values
     score = get_score(labels, preds)
@@ -21,4 +21,6 @@ def get_result(c, df, fold):
         mlflow.log_metric("score", score, fold)
     if c.wandb.enabled:
         wandb.log({"score": score, "fold": fold})
+        if loss is not None:
+            wandb.log({"loss": loss, "fold": fold})
     return score
